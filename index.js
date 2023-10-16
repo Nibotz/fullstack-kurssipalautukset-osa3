a91d958c507711b9d765488715806a36c6ce8ed5
Niilo Mikkola <niilo.mikkola@gmail.com>
2023-10-16 21:25:20 +0300

    jotain lisäyksiä

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('postData', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
@@ -96,7 +98,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
